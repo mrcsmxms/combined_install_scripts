69d89934c386df45a91b76f6fd0cc5d67ce53663
Marcus <mrcsmxms@MacBookPro-Marcus.fritz.box>
2016-11-27 01:32:08 +0100

    updated script

--- installAirPlay.sh
+++ installAirPlay.sh
@@ -1,3 +1,4 @@
+cd
 echo "
   ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
     ░░░░▒▒▒▒▒▒▒▒▒▒░░▒▒▒▒▒▒░░▒▒▒▒▒▒░░▒▒▒▒▒▒░░░░░░
@@ -15,9 +16,7 @@ echo "
                         // github.com/mrcsmxms
 "
 sudo apt-get update
-clear
 echo "Which output would you like to use for audio?"
-#echo "1: HeadPhones // 2: HDMI // 3: Auto"
 echo "Choose wisely!"
 echo "[1] Headphone / [2] HDMI / [3] Auto"
 read line
@@ -32,41 +31,25 @@ case "$line" in
        echo "audio output set to automatic"
         ;;
 esac
-sudo apt-get install git libao-dev libssl-dev libcrypt-openssl-rsa-perl libio-socket-inet6-perl libwww-perl avahi-utils
-sudo git clone https://github.com/albertz/shairport.git shairport
-cd shairport
+sudo apt-get install git libao-dev libssl-dev libcrypt-openssl-rsa-perl libio-socket-inet6-perl libwww-perl avahi-utils -y
+cd
+sudo git clone https://github.com/albertz/shairport.git /home/pi/shairport
+cd /home/pi/shairport
 sudo make
 sudo make install
+sudo cp /home/pi/shairport/shairport.init.sample /etc/init.d/shairport
+cd /etc/init.d
+sudo chmod a+x shairport
+sudo update-rc.d shairport defaults
 wget http://www.forum-raspberrypi.de/mydownloads/downloads/libnet-sdp-perl_0.07-1_all.deb
 wget http://www.forum-raspberrypi.de/mydownloads/downloads/shairport_0.05-1_armhf.deb
 sudo dpkg -i libnet-sdp-perl_0.07-1_all.deb
 sudo dpkg -i shairport_0.05-1_armhf.deb
-echo "Should AirPlay always start on boot?"
-echo "[Y/N]"
-read line
-case "$line" in
-    y|Y|J|j) sudo cp shairport.init.sample /etc/init.d/shairport
-             cd /etc/init.d
-             sudo chmod a+x shairport
-             sudo update-rc.d shairport defaults
-             sudo /etc/init.d/shairport start
-             echo "AirPlay started!"
-             echo "If you want to change the name of the AirPlay Station enter"
-             echo "'sudo nano /etc/init.d/shairport'"
-             echo "and enter '-a Name' after $PIDFILE"
-             echo "Changes will appyl after restarting the program by the command"
-             echo "'sudo /etc/init.d restart'"
-             echo "Have fun"
-        ;;
-    n|N)     cd /home/pi
-             sudo chmod a+x shairport
-             sudo /home/pi/shairport start
-             echo "AirPlay started!"
-             echo "If you want to change the name of the AirPlay Station enter"
-             echo "'sudo nano /home/pi/shairport'"
-             echo "and enter '-a Name' after $PIDFILE"
-             echo "Changes will appyl after restarting the program by the command"
-             echo "'sudo /home/pi restart'"
-             echo "Have fun"
-        ;;
-esac
+sudo /etc/init.d/shairport start
+echo "AirPlay started!"
+echo "If you want to change the name of the AirPlay Station enter"
+echo "'sudo nano /etc/init.d/shairport'"
+echo "and replace the variable 'Shairport'"
+echo "Changes will appyl after restarting the program by the command"
+echo "'sudo /etc/init.d restart'"
+echo "Have fun"
